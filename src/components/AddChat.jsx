f5a84df2ccf138ec11c8c30bf586f3b1f916c262
Godbluff <bal@udp.no>
2016-10-14 13:22:13 +0200

    full commit

--- src/components/AddChat.jsx
+++ src/components/AddChat.jsx
@@ -1,99 +1,51 @@
-const axios = require('axios');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
 const AddChat = React.createClass({
-  propTypes: {
-    url: React.PropTypes.string.isRequired
-    /*
+    propTypes: {
+        postMessage: React.PropTypes.func.isRequired
+    },
 
-      1. Use proptypes to make sure that
-      we require a prop 'url' that should
-      be a string
+    addChatMessage(name, text) {
+        if (this.refs.newChatInput.value) {
+            this.props.postMessage({ name, text })
+        }
+    },
 
-     */
-  },
+    handleSubmit(e){
+        if (!this.refs.newChatInput.value) {
+            return;
+        }
 
-  addChatMessage() {
-    const chatValue = this.refs.newChatInput.value;
-    const nameValue = this.refs.newNameInput.value;
-    console.log(chatValue);
-    axios.post('https://react-workshop-chat.herokuapp.com/chat', {
-      name: nameValue,
-      text: chatValue
-    })
-        .catch(function (error) {
-          console.log(error);
-        });
-    /*
+        if (e.keyCode === 13) {
+            this.addChatMessage(
+                this.refs.nicknameInput.value,
+                this.refs.newChatInput.value
+            );
 
-      2. Use ReactDOM.findDOMNode to get
-      the input field and the value
+            this.refs.newChatInput.value = '';
+        }
+    },
 
-      3. Use a HTTP request library like
-      super-agent, axios, fetch or jQuery.ajax
-      to do a POST-request to the url supplied.
-      The API accepts data of the form:
-
-      {
-        text: 'My new chat message!'
-        name: 'Anonymous'
-      }
-
-      4. Reset the input field
-
-     */
-  },
-
-  handleSubmit(e) {
-    if (e.key === 'Enter') {
-      console.log('Submitting to addChatMessage...');
-      this.addChatMessage();
+    render(){
+        return (
+            <div className="form-group">
+                <input
+                    type="text"
+                    placeholder="Nickname"
+                    ref="nicknameInput"
+                    className="form-control"
+                />
+                <input
+                    type="text"
+                    placeholder="Compose Message"
+                    ref="newChatInput"
+                    className="form-control"
+                    onKeyDown={this.handleSubmit}
+                />
+            </div>
+        );
     }
-    /*
-
-      5. Handle when the user presses
-      the ENTER key in the input field
-
-     */
-  },
-
-  render(){
-    return (
-      <div className="form-group">
-
-        {/*
-
-          6. Attach an onKeyDown listener
-          to the input field that calls
-          you handleSubmit function
-
-        */}
-
-        <input
-            type="text"
-            placeholder="Your Name Here"
-            ref="newNameInput"
-            className="form-control"
-
-        />
-        <input
-            type="text"
-            placeholder="Compose Message"
-            ref="newChatInput"
-            className="form-control"
-            onKeyDown={this.handleSubmit}
-        />
-
-        {/*
-
-          7. Add an additional input field
-          so that you can supply a nickname/name as well
-
-        */}
-      </div>
-    );
-  }
 });
 
 module.exports = AddChat;
--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -1,86 +1,27 @@
-const axios = require('axios');
 const React = require('react');
 const SetIntervalMixin = require('../mixins/set-interval-mixin');
 
 const ChatList = React.createClass({
-  propTypes : {
-    url: React.PropTypes.string.isRequired
-  },
   mixins: [SetIntervalMixin],
-  /*
 
-    1. Use proptypes to make sure that
-    we require a prop 'url' that should
-    be a string
-
-    2. Make sure you add any appropriate
-    mixins that you need to use in your
-    application
-
-   */
-
-  getInitialState() {
-    return {
-      chats: []
-    };
-  },
-
-  getChats() {
-    console.log('Fetching chat...');
-    axios
-        .get('https://react-workshop-chat.herokuapp.com/chat')
-        .then(function (response) {
-          this.setState({
-            chats: response.data
-          });
-        }.bind(this))
-        .catch(function (error) {
-          console.log('Damn: ' + error);
-        });
-    /*
-
-      3. Create a method that uses an HTTP request
-      library like super-agent, axios, fetch or jQuery.ajax
-      to fetch chat messages from the API URL
-
-      The API returns a response in the form:
-
-      [
-        { text: 'A chat message' },
-        { text: 'Another chat message' },
-        ...
-      ]
-
-      Use this data to update the state
-      of your application
-
-     */
+  propTypes: {
+    populateMessages: React.PropTypes.func.isRequired,
+    messages: React.PropTypes.array.isRequired
   },
 
   componentDidMount() {
     this._setInterval(this.getChats, 1000);
-    /*
+  },
 
-      4. Use the SetIntervalMixin to
-      regularly call the getChats method
-      to update the chat messages of
-      you application
-
-     */
+  getChats() {
+    this.props.populateMessages();
   },
 
   render() {
-    let list = this.state.chats.map((item, index) => {
+    let list = this.props.messages.map((item, index) => {
       return (
-          /*
-
-           5. The chat message items also supply a name (item.name).
-           Print that out to the UI as well
-
-           */
-
           <li className="list-group-item" key={item._id} >
-            {item.name} : {item.text}
+            <span>{item.name}: </span>{item.text}
           </li>
       );
     });
