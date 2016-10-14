960ba81d4be49447b2864d2b848091fb47bab303
Godbluff <bal@udp.no>
2016-10-14 13:42:04 +0200

    full commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,67 @@
 const React = require('react');
+const { connect } = require('react-redux');
+const { bindActionCreators } = require('redux');
+
 const AddChat = require('./AddChat.jsx');
 const ChatList = require('./ChatList.jsx');
 
+const {
+    populateMessages,
+    postMessage,
+} = require('../actions');
+
 const App = React.createClass({
   render() {
+    const {
+        postMessage,
+        populateMessages,
+        messages
+    } = this.props;
+
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            <h1 className="text-center"> React Workshop Chat </h1>
-            <div className="col-sm-8 col-sm-offset-2">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <h1 className="text-center"> React Workshop Chat </h1>
+              <div className="col-sm-8 col-sm-offset-2">
 
-              <AddChat
-                url="https://react-workshop-chat.herokuapp.com/chat"
-              />
-
-            </div>
-            <div className="col-sm-10 col-sm-offset-1">
-            <div className="panel panel-default">
-              <div className="panel-heading">Chat Messages</div>
-
-                <ChatList
-                  url="https://react-workshop-chat.herokuapp.com/chat"
+                <AddChat
+                    postMessage={postMessage}
                 />
 
+              </div>
+              <div className="col-sm-10 col-sm-offset-1">
+                <div className="panel panel-default">
+                  <div className="panel-heading">Chat Messages</div>
+
+                  <ChatList
+                      messages={messages}
+                      populateMessages={populateMessages}
+                  />
+
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
     )
   }
 });
 
-module.exports = App;
+function mapStateToProps(state) {
+  return {
+    messages: state.messages
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    populateMessages,
+    postMessage
+  }, dispatch);
+}
+
+module.exports = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
